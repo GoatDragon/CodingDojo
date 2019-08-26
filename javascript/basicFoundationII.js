93a4c56a868302869ee0f42319d53d107a31ddc2
Sam Jackson <sj1@usa.net>
2019-08-26 08:35:07 -0500

    more javascript work

--- javascript/basicFoundationII.js
+++ javascript/basicFoundationII.js
@@ -1,4 +1,4 @@
-    // Biggie Size - Given an array, write a function that changes all positive numbers in the array to the string "big".  Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1, "big", "big", -5].
+// Biggie Size - Given an array, write a function that changes all positive numbers in the array to the string "big".  Example: makeItBig([-1,3,5,-5]) returns that same array, changed to [-1, "big", "big", -5].
 function biggieSize(arr){
     for (i=0; i<arr.length; i++){
         if(arr[i]>0){
@@ -8,6 +8,7 @@ function biggieSize(arr){
     return arr
 }
 //console.log(biggieSize([-1,3,5,-5]))
+
     // Print Low, Return High - Create a function that takes in an array of numbers.  The function should print the lowest value in the array, and return the highest value in the array.
 function printLowReturnHigh(arr){
     var min = arr[0]
@@ -24,6 +25,7 @@ function printLowReturnHigh(arr){
     return max
 }
 //console.log(printLowReturnHigh([-1,3,5,-5]))
+
     // Print One, Return Another - Build a function that takes in an array of numbers.  The function should print the second-to-last value in the array, and return the first odd value in the array.
 function printOneReturnAnother(arr){
     console.log(arr[arr.length-2])
@@ -34,6 +36,7 @@ function printOneReturnAnother(arr){
     }
 }
 //console.log(printOneReturnAnother([2,4,6,8,9,10,11,15]))
+
     // Double Vision - Given an array (similar to saying 'takes in an array'), create a function that returns a new array where each value in the original array has been doubled.  Calling double([1,2,3]) should return [2,4,6] without changing the original array.
 function doubleVision(arr){
     for (i=0; i<arr.length; i++){
@@ -42,6 +45,7 @@ function doubleVision(arr){
     return arr
 }
 //console.log(doubleVision([1,2,3]))
+
     // Count Positives - Given an array of numbers, create a function to replace the last value with the number of positive values found in the array.  Example, countPositives([-1,1,1,1]) changes the original array to [-1,1,1,3] and returns it.
 function countPositives(arr){
     var total = 0
@@ -54,6 +58,7 @@ function countPositives(arr){
     return arr
 }
 //console.log(countPositives([-1,1,1,1]))
+
     // Evens and Odds - Create a function that accepts an array.  Every time that array has three odd values in a row, print "That's odd!".  Every time the array has three evens in a row, print "Even more so!".
 function evensAndOdds(arr){
     var oddCounter = 0
@@ -78,19 +83,85 @@ function evensAndOdds(arr){
     }
 }
 //evensAndOdds([1,2,3,33,71,4,5,6,882,16,7])
+
     // Increment the Seconds - Given an array of numbers arr, add 1 to every other element, specifically those whose index is odd (arr[1], arr[3], arr[5], etc).  Afterward, console.log each array value and return arr.
+function incrementTheSeconds(arr){
+    for(i=1;i<arr.length;i+=2){
+        arr[i]++
+        console.log(arr[i])
+    }
+    return arr
+}
+//console.log(incrementTheSeconds([0,1,2,3,4,5,6,7,8,9]))
 
     // Previous Lengths - You are passed an array (similar to saying 'takes in an array' or 'given an array') containing strings.  Working within that same array, replace each string with a number - the length of the string at the previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4]. Hint: Can for loops only go forward?
+var arr= ["hello", "dojo", "awesome"]
+function previousLengths(arr){
+    for(i=arr.length-1;i>0;i--){
+        arr[i]=arr[i-1].length
+    }
+    return arr
+}
+//console.log(previousLengths(arr))
 
     // Add Seven - Build a function that accepts an array. Return a new array with all the values of the original, but add 7 to each. Do not alter the original array.  Example, addSeven([1,2,3]) should return [8,9,10] in a new array.
+var arr=[1,2,3,4,5,6,7,8,9,10]
+function addSeven(arr){
+    for(i=0;i<arr.length;i++){
+        arr[i]=arr[i]+7
+    }
+    return arr
+}
+//console.log(addSeven(arr))
 
     // Reverse Array - Given an array, write a function that reverses its values, in-place.  Example: reverse([3,1,6,4,2]) returns the same array, but now contains values reversed like so... [2,4,6,1,3].  Do this without creating an empty temporary array.  (Hint: you'll need to swap values).
+function reverseArray(arr){
+    for (i=0;i<arr.length/2;i++){
+        temp=arr[i]
+        arr[i]=arr[arr.length-i-1]
+        arr[arr.length-i-1]=temp
+    }
+    return arr
+}
+//console.log(reverseArray(arr))
 
     // Outlook: Negative - Given an array, create and return a new one containing all the values of the original array, but make them all negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
+function outlookNegative(arr){
+    for(i=0;i<arr.length;i++){
+        if(arr[i]>0){
+            arr[i]=arr[i]*(-1)
+        }
+    }
+    return arr
+}
+//console.log(outlookNegative([-1,-2,-4,5,6,-10,7,21]))
 
     // Always Hungry - Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food".  If no array values are "food", then print "I'm hungry" once.
+function alwaysHungry(arr){
+    for(i=0;i<arr.length;i++){
+        if(arr[i]=='food'){
+            console.log('yummy');
+            var fed = true;
+        }
+    }
+    if (fed != true){
+        console.log("I'm hungry");
+    }
+}
+//alwaysHungry([1,2,3,4, 'food'])
 
     // Swap Toward the Center - Given an array, swap the first and last values, third and third-to-last values, etc.  Example: swapTowardCenter([true,42,"Ada",2,"pizza"]) turns the array into ["pizza", 42, "Ada", 2, true].  swapTowardCenter([1,2,3,4,5,6]) turns the array into [6,2,4,3,5,1].  No need to return the array this time.
+function swapTowardtheCenter(arr){
+    temp=arr[0]
+    arr[0]=arr[arr.length-1]
+    arr[arr.length-1]=temp
+    temp=arr[2]
+    arr[2]=arr[arr.length-3]
+    arr[arr.length-3]=temp
+    return arr
+}
+//console.log(swapTowardtheCenter([true,42,"Ada",2,"pizza"]))
+//console.log(swapTowardtheCenter([1,2,3,4,5,6]))
 
     // Scale the Array - Given an array arr and a number num, multiply all values in the array arr by the number num, and return the changed array arr.  For example, scaleArray([1,2,3], 3) should return [3,6,9].
 function scaleArray(arr, num){
@@ -99,4 +170,4 @@ function scaleArray(arr, num){
     }
     return arr
 }
-console.log(scaleArray([1,2,3], 3))
+//console.log(scaleArray([1,2,3], 3))
--- javascript/intermediateFoundation.js
+++ javascript/intermediateFoundation.js
@@ -1,10 +1,39 @@
     // Sigma - Implement function sigma(num) that, given a number, returns the sum of all positive integers up to the given number (inclusive).  Ex: sigma(3) = 6 (or 1+2+3); sigma(5) = 15 (or 1+2+3+4+5).
+function sigma(num){
+	var sum = 0
+	for(i=1;i<=num;i++){
+		sum += i
+	}
+	return sum
+}
+//console.log(sigma(100))
 
     // Factorial - Write a function factorial(num) that, given a number, returns the product (multiplication) of all positive integers from 1 up to the given number (inclusive).  For example, factorial(3) = 6 (or 1*2*3); factorial(5) = 120 (or 1*2*3*4*5).
+function factorial(num){
+	var mult = 1
+	for(i=1;i<=num;i++){
+		mult *= i
+	}
+	return mult
+}
+//console.log(factorial(5))
 
     // Fibonacci - Create a function to generate Fibonacci numbers.  In this famous mathematical sequence, each number is the sum of the previous two, starting with values 0 and 1.  Your function should accept one argument, an index into the sequence (where 0 corresponds to the initial value, 4 corresponds to the value four later, etc).  Examples: fibonacci(0) = 0 (given), fibonacci(1) = 1 (given), fibonacci(2) = 1 (fib(0)+fib(1), or 0+1), fibonacci(3) = 2 (fib(1) + fib(2)3, or 1+1), fibonacci(4) = 3 (1+2), fibonacci(5) = 5 (2+3), fibonacci(6) = 8 (3+5), fibonacci(7) = 13 (5+8).  Do this without using recursion first.  If you don't know what a recursion is yet, don't worry as we'll be introducing this concept in Part 2 of this assignment.
+function fibonacci(index){
+	var fibarr = [0, 1]
+	for(i=2;i<=index;i++){
+		fibarr[i] = fibarr[i-1] + fibarr[i-2]
+	}
+	// console.log(fibarr);
+	return fibarr[index]
+}
+//console.log(fibonacci(20));
 
     // Array: Second-to-Last: Return the second-to-last element of an array. Given [42, true, 4, "Liam", 7], return "Liam".  If array is too short, return null.
+function secondToLast(arr){
+	return arr[arr.length-2]
+}
+console.log(secondToLast([1]))
 
     // Array: Nth-to-Last: Return the element that is N-from-array's-end.  Given ([5,2,3,6,4,9,7],3), return 4.  If the array is too short, return null.
 
