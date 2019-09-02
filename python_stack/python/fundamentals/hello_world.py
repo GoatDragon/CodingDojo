e8701369f1928112afd291f18c1ca35b5374be05
Sam Jackson <sj1@usa.net>
2019-09-02 10:12:51 -0500

    hello world python

--- python_stack/python/fundamentals/hello_world.py
+++ python_stack/python/fundamentals/hello_world.py
@@ -5,3 +5,8 @@ print('Hello '+ name)
 favorite_num = 42
 print('Hello',favorite_num)
 print('Hello '+str(favorite_num))
+fav1 = 'Steak'
+fav2 = 'Ice Cream'
+print('I love to eat {} and {}'.format(fav1, fav2))
+print(f'I love to eat {fav1} and {fav2}')
+print(f'I love to eat {fav1.upper()} and {fav2.title()}')
