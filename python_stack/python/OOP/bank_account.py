c5ed408a8d3937a5c74db23c9571238ca6c829a1
Sam Jackson <sj1@usa.net>
2019-09-04 13:03:41 -0500

    add bank account oop project

---
+++ python_stack/python/OOP/bank_account.py
@@ -0,0 +1,28 @@
+class BankAccount:
+    def __init__(self, name, int_rate, balance):
+        self.name = name
+        self.int_rate = int_rate
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+
+    def display_account_info(self):
+        print(f"Balance is ${self.balance} with {int(self.int_rate*100)}% interest")
+        return self
+
+    def yield_interest(self):
+        self.balance += self.balance * (self.int_rate)
+        return self
+
+
+act1 = BankAccount("account 1", 0.02, 1000)
+act2 = BankAccount("account 2", 0.05, 100)
+
+act1.deposit(10).deposit(20).deposit(30).withdraw(5).yield_interest().display_account_info()
+act2.deposit(100).deposit(200).withdraw(10).withdraw(20).withdraw(50).withdraw(70).yield_interest().display_account_info()
--- python_stack/python/OOP/user.py
+++ python_stack/python/OOP/user.py
@@ -6,41 +6,32 @@ class User:
 
     def make_deposit(self, amount):
         self.account_balance += amount
+        return self
 
     def make_withdrawal(self, amount):
         self.account_balance -= amount
+        return self
 
     def display_user_balance(self):
         print(f"{self.name}, Balance ${self.account_balance}")
+        return self
 
     def transfer_money(self, amount, receiver):
         print(f"transfering ${amount} from {self.name} to {receiver.name}")
         self.account_balance -= amount
         receiver.account_balance += amount
+        return self
 
 
 guido = User("Guido van Rossum", "guido@python.com")
 monty = User("Monty Python", "monty@python.com")
 goat = User("Billy Bob", "goat@dragon.com")
 
-guido.make_deposit(100)
-guido.make_deposit(20)
-guido.make_deposit(30)
-guido.make_withdrawal(15)
-guido.display_user_balance()
+guido.make_deposit(100).make_deposit(20).make_deposit(30).make_withdrawal(15).display_user_balance()
 
-monty.make_deposit(5)
-monty.make_deposit(10)
-monty.make_withdrawal(2)
-monty.make_withdrawal(4)
-monty.display_user_balance()
+monty.make_deposit(5).make_deposit(10).make_withdrawal(2).make_withdrawal(4).display_user_balance()
 
-goat.make_deposit(1000)
-goat.make_withdrawal(5)
-goat.make_withdrawal(10)
-goat.make_withdrawal(15)
-goat.display_user_balance()
+goat.make_deposit(1000).make_withdrawal(5).make_withdrawal(10).make_withdrawal(15).display_user_balance()
 
-guido.transfer_money(100, goat)
-guido.display_user_balance()
+guido.transfer_money(100, goat).display_user_balance()
 goat.display_user_balance()
