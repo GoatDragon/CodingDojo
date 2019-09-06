4ceb5e6e39ef5484d4be2fc29c6c6881efc820d6
Sam Jackson <sj1@usa.net>
2019-09-06 16:37:05 -0500

    add html table project

--- python_stack/flask/fundamentals/hello_flask/hello_flask.py
+++ python_stack/flask/fundamentals/hello_flask/hello_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask, render_template  # Import Flask to allow us to create our app
 app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 
@@ -17,6 +17,17 @@ def success():
 def hello(name):
     print(name)
     return "Hello, " + name
+
+
+@app.route('/lists')
+def render_lists():
+    # Soon enough, we'll get data from a database, but for now, we're hard coding data
+    student_info = [
+        {'name': 'Michael', 'age': 35},
+        {'name': 'John', 'age': 30},
+        {'name': 'Mark', 'age': 25},
+        {'name': 'KB', 'age': 27}]
+    return render_template("lists.html", random_numbers=[3, 1, 5], students=student_info)
 
 
 # app.run(debug=True) should be the very last statement!
