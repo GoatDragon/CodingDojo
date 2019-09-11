0f1fa70d8e3e744a289a9320b6802ad7447c044d
Sam Jackson <sj1@usa.net>
2019-09-11 16:13:19 -0500

    add time display project

---
+++ python_stack/django/intro/time_display/apps/clock/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from time import gmtime, strftime
+
+
+def index(request):
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
+    }
+    return render(request, 'clock/index.html', context)
