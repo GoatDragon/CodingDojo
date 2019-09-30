a9c8595b60c9b12ea1e057e02a34f94b17b10419
Sam Jackson <sj1@usa.net>
2019-09-30 14:13:41 -0500

    add guess the number

---
+++ ruby_stack/arrays.rb
@@ -0,0 +1,34 @@
+array = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+# # .at or .fetch
+    # puts array.at 0
+    # puts array.fetch 1
+
+# .delete
+    # puts array.delete 'f'
+    # puts array
+
+# .reverse
+    # array.reverse!
+    # puts array
+
+# .length
+    # puts array.length
+
+# .sort
+    # puts array.sort
+
+# .slice
+    # puts array.slice 4..5
+
+# .shuffle
+    # puts array.shuffle
+
+# .join
+    # puts array.join '<=>'
+
+# .insert
+    # puts array.insert 0, 99
+
+# values_at() -> returns an array with values specified in the param
+    # puts array.values_at(1..4)
