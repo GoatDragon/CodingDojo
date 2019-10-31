a1f1d6319afef74d858bd59a3be14cd3d50ced8c
Sam Jackson <sj1@usa.net>
2019-10-31 12:09:01 -0500

    add landing page assignment

--- MEAN_stack/JS_fundamentals/test.js
+++ MEAN_stack/JS_fundamentals/test.js
@@ -1,16 +1,1 @@
-class Pizza {
-    constructor(radius, slices) {
-        this.radius = radius;
-        this._slices = slices;
-    }
-    get slices () { 
-        return this._slices; 
-    }
-    set slices (slices) { 
-        this._slices = slices;
-    }
-};
-const pie = new Pizza(12, 6);
-console.log(pie.slices);     // we use the getter to determine how many slices we have
-pie.slices = 12;             // the setter will change the number of slices
-console.log(pie.slices);     // use the getter again to make sure our slices have doubled
+console.log('I am running from node!');
