ac1ff9266e5c6b5ddc6e940e88b901169a4c8655
Sam Jackson <sj1@usa.net>
2020-01-21 11:27:21 -0600

    restful task forums

--- MEAN_stack/angular/restful_tasks/public/src/app/app.component.ts
+++ MEAN_stack/angular/restful_tasks/public/src/app/app.component.ts
@@ -8,21 +8,48 @@ import { HttpService } from './http.service';
 })
 export class AppComponent implements OnInit {
   title: string = 'restful tasks';
-  tasks:  any = [];
-  detailedTasks: any = [];
+  tasks: any = [];
+  newTask: any;
+  editTask: any = false;
 
   constructor(private _httpService: HttpService) {};
 
   ngOnInit() {
-    // this.getTasksFromService()
+    this.newTask = {title: "", description: ""};
   };
 
   getTasksFromService() {
-    let observable = this._httpService.getTasks()
-        observable.subscribe( data => { this.tasks = data })
+    let observable = this._httpService.getTasks();
+        observable.subscribe( data => { this.tasks = data });
   };
   getTaskDetails(id) {
-    let observable = this._httpService.findTask(id)
-        observable.subscribe( data => { this.detailedTasks.push(data) });
+    let observable = this._httpService.findTask(id);
+        observable.subscribe( data => { console.log(data) });
+  };
+  onSubmit(params) {
+    this._httpService.addTask(this.newTask)
+        .subscribe( data => {
+            this.newTask = {title: "", description: ""}
+            this.getTasksFromService()
+        });
+  };
+  deleteTask(taskID) {
+    this._httpService.deleteTask(taskID)
+        .subscribe( data => {
+            console.log('deleted', data)
+            this.getTasksFromService()
+            });
+  };
+  updateTask(taskID) {
+    if (this.editTask === false) {
+        let observable = this._httpService.findTask(taskID);
+        observable.subscribe( data => { this.editTask = data });
+    } else {
+        this._httpService.updateTask(taskID, this.editTask)
+            .subscribe( data => {
+                this.editTask = false;
+                this.getTasksFromService();
+            });
+    };
   };
 };
--- MEAN_stack/angular/restful_tasks/public/src/app/app.module.ts
+++ MEAN_stack/angular/restful_tasks/public/src/app/app.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [HttpService],
